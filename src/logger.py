94fb79e25fa9e1daa07dd488b37d74813d9ead55
bmondal98 <biswajit00062@gmail.com>
2023-10-23 10:41:24 +0530

    directory structure added

---
+++ src/logger.py
@@ -0,0 +1,26 @@
+'''
+    for logging
+'''
+
+import logging
+import os
+import sys
+from datetime import datetime
+from constants import projectConstant
+
+
+
+LOG_FOLDER = f"{projectConstant.PROJECT_NAME}_LOGS"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),LOG_FOLDER,LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
+    level=logging.INFO
+)
