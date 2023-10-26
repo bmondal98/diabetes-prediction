ea89b1d76bf7c1976d065f7a3670d6bfeb434224
bmondal98 <biswajit00062@gmail.com>
2023-10-26 16:09:37 +0530

    logger modified

--- src/logger.py
+++ src/logger.py
@@ -11,16 +11,22 @@ from constants import projectConstant
 
 
 LOG_FOLDER = f"{projectConstant.PROJECT_NAME}_LOGS"
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M')}.log"
+#f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),LOG_FOLDER)
-os.makedirs(logs_path, exist_ok=True)
+
+if not os.path.isfile(logs_path):
+    os.makedirs(logs_path, exist_ok=True)
 
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    # filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s -%(message)s",
-    level=logging.INFO
+    level=logging.INFO,
+    handlers=[logging.FileHandler(LOG_FILE_PATH),
+                              logging.StreamHandler()]
+
 )
